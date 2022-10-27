06d9c7a4a2096984d73132fdf5f8065a43a2dfaa
Hugo S. Dias <hugo_sdias@hotmail.com>
2022-10-27 20:40:06 -0300

    primeiro commit, cadastrado estrutura da conta e os métodos

---
+++ Enum/TipoConta.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Transferencia_Bancaria
+{
+    public enum TipoConta  
+    {
+         PessoaFisica = 1,
+        PessoaJuridica = 2
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using Transferencia_Bancaria;
+
+Conta minhaConta = new Conta(TipoConta.PessoaFisica, 0, 0, "Hugo S. Dias");
+
+Console.WriteLine(minhaConta.ToString());
